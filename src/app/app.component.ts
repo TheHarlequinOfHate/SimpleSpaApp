75d9a2db9a488e9a12ce41aa2279f4724c01a6dc
Joker <george_02@abv.bg>
2024-12-04 19:28:13 +0200

    created footer and home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavBarComponent ],
+  imports: [RouterOutlet , NavBarComponent, FooterComponent, HomePageComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
